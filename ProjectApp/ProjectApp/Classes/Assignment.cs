10a462703833211f617641655fbd2ae308779d89
Jozo Meštrović <jozomestrovic0@gmail.com>
2024-11-21 15:24:04 +0100

    Predaja zadnjeg zadatka

---
+++ ProjectApp/ProjectApp/Classes/Assignment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectApp.Enums;
+
+namespace ProjectApp.Classes
+{
+    public class Assignment
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DateTime Deadline { get; set; }
+        public assignmentStatus Status { get; set; }
+        public assignmentPriority Priority { get; set; }
+        public double expectedDurationMinutes { get; set; }
+        public Project parentProject { get; set; }
+        public Guid id { get;}
+        public Assignment(string name, string desc, DateTime deadlineDate, double minutes, Project project, assignmentPriority priority)
+        {
+            Name = name;
+            Description = desc;
+            Deadline = deadlineDate;
+            Status = assignmentStatus.Active;
+            expectedDurationMinutes = minutes;
+            parentProject = project;
+            Priority = priority;
+            id = Guid.NewGuid();
+        }
+        public void toActive()
+        {
+            this.Status = assignmentStatus.Active;
+        }
+        public void toDelayed()
+        {
+            this.Status = assignmentStatus.Delayed;
+        }
+        public void toCompleted()
+        {
+            this.Status = assignmentStatus.Completed;
+        }
+    }
+}
---
+++ ProjectApp/ProjectApp/Classes/Project.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectApp.Enums;
+namespace ProjectApp.Classes
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime startDate { get; }
+        public DateTime endDate { get; set; }
+        public projectStatus status { get; set; }
+        public Guid id { get;}
+        public Project(string name, string desc, DateTime end) {
+            Name = name;
+            Description = desc;
+            startDate = DateTime.Now;
+            endDate = end;
+            status = projectStatus.Active; //it's intuitive that a new project is set as active
+            id = Guid.NewGuid();
+        }
+
+        public void toActive(){
+            this.status = projectStatus.Active;
+        }
+        public void toOnHold()
+        {
+            this.status = projectStatus.OnHold;
+        }
+        public void toCompleted()
+        {
+            this.status = projectStatus.Completed;
+        }
+
+    }
+}
